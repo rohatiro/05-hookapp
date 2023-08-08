b6e63797e1221f382fbe698e1ef71b871ee95cf3
Roberto Tienda <73715918+robertotr@users.noreply.github.com>
2023-08-08 10:41:39 -0600

    class - useLayoutEffect

--- src/03-examples/Pokemon.jsx
+++ src/03-examples/Pokemon.jsx
@@ -1,8 +1,23 @@
+import { useLayoutEffect, useRef, useState } from "react"
+
 export const Pokemon = ({ name, sprites }) => {
+    const nameRef = useRef();
+    const [boxSize, setBoxSize] = useState({ width: 0, height: 0 });
+
+    useLayoutEffect(() => {
+        const { width, height } = nameRef.current.getBoundingClientRect();
+        setBoxSize({ width, height })
+    }, []);
+
     return (
-        <div>
-            <h2>{name}</h2>
-            <img src={sprites?.front_default} alt={name} />
-        </div>
+        <>
+            <div className="d-flex">
+                <h2 ref={nameRef}>{name}</h2>
+                <img src={sprites?.front_default} alt={name} />
+            </div>
+            <pre>
+                { JSON.stringify(boxSize) }
+            </pre>
+        </>
     )
 }
